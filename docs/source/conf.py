1adef5cb90b154f00e9d1dd3eb0e043e3e37df18
hgloeckner <helene.gloeckner@posteo.de>
2024-08-25 01:17:28 +0200

    added authors

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,7 @@ sys.path.insert(0, os.path.abspath("../pydropsonde"))
 
 project = "pydropsonde"
 copyright = "2024, Geet George, Helene Gloeckner, Theresa Mieslinger, Nina Robbins"
-author = "Geet George, Helene Gloeckner, Theresa Mieslinger, Nina Robbins"
+author = "Geet George, Henning Dorff, Helene Gloeckner, Lukas Kluft, Tobias Koelling, Theresa Mieslinger, Nina Robbins"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
