b219b58495c7d891e5e26f79f848feccb021c75b
Nina Robbins Blanch <nina.robbins@mpimet.mpg.de>
2024-09-27 12:17:05 -0400

    first try at Level 4 filename and dir

--- pydropsonde/processor.py
+++ pydropsonde/processor.py
@@ -1235,6 +1235,7 @@ class Sonde:
 @dataclass(order=True)
 class Gridded:
     sondes: dict
+    circles: dict = None
 
     def concat_sondes(self):
         """
@@ -1244,7 +1245,7 @@ class Gridded:
         self._interim_l3_ds = xr.concat(list_of_l2_ds, dim="sonde_id", join="exact")
         return self
 
-    def get_all_attrs(self):
+    def get_all_sonde_attrs(self):
         attrs = set()
         for sonde in list(self.sondes.values()):
             attrs = set(sonde.attrs) | attrs
@@ -1383,4 +1384,34 @@ class Gridded:
         self.platform_ids = platform_ids
         self.flight_ids = flight_ids
 
+        return self
+
+    def get_l4_dir(self, l4_dir: str = None):
+        if l4_dir:
+            self.l4_dir = l4_dir
+        elif self.circles is not None:
+            self.l4_dir = (
+                list(self.circles.values())[0]
+                .l2_dir.replace("Level_2", "Level_4")
+                .replace(list(self.circles.values())[0].flight_id, "")
+                .replace(list(self.circles.values())[0].platform_id, "")
+            )
+        else:
+            raise ValueError("No circles and no l4 directory given, cannot continue")
+        return self
+
+    def get_l4_filename(self, l4_filename: str = None):
+        if l4_filename is None:
+            l4_filename = hh.l4_filename
+        else:
+            l4_filename = l4_filename
+
+        self.l4_filename = l4_filename
+        return self
+
+    def add_l4_ds(self, l4_dir: str = None):
+        if l4_dir is None:
+            self.l4_ds = self._interim_l4_ds.copy()
+        else:
+            self.l4_ds = xr.open_dataset(l4_dir)
         return self
