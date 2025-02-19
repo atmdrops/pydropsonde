d26d3f1d5c30657624afe5a793f07ca87743e2eb
hgloeckner <helene.gloeckner@posteo.de>
2025-02-19 14:50:30 +0100

    split circle variables and xy calculation

--- pydropsonde/pipeline.py
+++ pydropsonde/pipeline.py
@@ -601,7 +601,11 @@ pipeline = {
     "prepare_circle_dataset": {
         "intake": "gridded",
         "apply": iterate_Circle_method_over_dict_of_Circle_objects,
-        "functions": ["drop_m_N_vars", "get_xy_coords_for_circles"],
+        "functions": [
+            "drop_m_N_vars",
+            "get_xy_coords_for_circles",
+            "add_circle_variables_to_ds",
+        ],
         "output": "gridded",
         "comment": "prepare circle dataset for calculation",
     },
