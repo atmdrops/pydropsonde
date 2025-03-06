d75a68dda607c8c4351b000a370f9187291d50d2
hgloeckner <helene.gloeckner@posteo.de>
2025-03-06 17:47:53 +0100

    add circle_id

--- pydropsonde/pipeline.py
+++ pydropsonde/pipeline.py
@@ -650,6 +650,7 @@ pipeline = {
             "add_vorticity",
             "add_omega",
             "add_wvel",
+            "add_circle_id_variable",
             "add_circle_variables_to_ds",
             "add_regression_stderr",
         ],
