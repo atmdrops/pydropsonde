70f80a3dfab4921fe887634ee782d2f17c99cf48
hgloeckner <helene.gloeckner@posteo.de>
2026-04-13 12:14:18 +0200

    fix tests

--- tests/publish_test.py
+++ tests/publish_test.py
@@ -1,6 +1,5 @@
 import pydropsonde.helper.__init__ as hh
 import pydropsonde.helper.rawreader as hr
-import pydropsonde.helper.paths as hp
 import pydropsonde.processor as pp
 import pydropsonde.pipeline as pl
 import pydropsonde
@@ -13,6 +12,6 @@ def test_version():
 def test_imports():
     hh.calc_iwv
     hr.check_launch_detect_in_afile
-    hp.path_to_flight_ids
+    hh.path_to_flight_ids
     pp.Sonde
     pl.apply_method_to_dataset
